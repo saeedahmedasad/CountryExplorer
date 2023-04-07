8eaefaa4da2c7b4e9af95b769555e40525742e65
Saeed Ahmed <phoolisahab@gmail.com>
2023-04-08 04:30:54 +0500

    add info page html

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -15,7 +15,67 @@ const Info = () => {
         />
         <button className="get_data">Get Info</button>
       </div>
-      <div className="info"></div>
+      <div className="info">
+        <div className="heading">Country Name</div>
+        <div className="left">
+          <img src="" alt=" " />
+          <p>Countries Full Name</p>
+        </div>
+        <div className="right">
+          <div className="sub_heading">Basic Info</div>
+          <div className="basic_info">
+            <div className="left">
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            </div>
+            <div className="right">
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            <div><span className="property">Name:</span><span className="value">Pakistan</span></div>
+            </div>
+          </div>
+        </div>
+        <div className="other_info">
+          <div className="sub_heading">Other Info</div>
+          <div className="cards">
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+            <div className="card">
+              <h5>Heading</h5>
+              <span>Bottom</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
