c1eb6f5a38486ca62f57675a0dda5cfe7c08985e
Saeed Ahmed <phoolisahab@gmail.com>
2023-04-08 00:03:38 +0500

    add info page and route

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./css/info.css"
 
 const Info = () => {
+  const [countryInput, setCountryInput] = useState("");
   return (
-    <div>Info</div>
-  )
-}
+    <>
+      <div className="input">
+        <input
+          type="text"
+          value={countryInput}
+          onChange={(e) => setCountryInput(e.target.value)}
+          placeholder="Enter Country Name"
+          className="country_input"
+        />
+        <button className="get_data">Get Info</button>
+      </div>
+      <div className="info"></div>
+    </>
+  );
+};
 
-export default Info
+export default Info;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./css/welcome.css"
+import {useNavigate} from "react-router-dom"
 
 const Welcome = () => {
+  const navigate = useNavigate()
   return (
     <div className="welcome">
         <div className="box">
             <div className="title">Enter Name of the <b>Country</b> and Get <b>Information</b> about it</div>
-            <button className="btn">Get Started</button>
+            <button className="btn" onClick={()=>navigate("/info")}>Get Started</button>
         </div>
     </div>
   )
